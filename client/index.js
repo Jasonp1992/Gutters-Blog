0d43c473bb1cc379ed62a6e86a61fc3034b4611e
jason <potvinj@gmail.com>
2023-03-17 18:13:34 -0400

    add provider

--- client/index.js
+++ client/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import Main from "./Main";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
-  <Router>
-    <Main />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Main />
+    </Router>
+  </Provider>
 );
