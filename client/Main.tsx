500a36ddb4d1b8bf2c9df1b9f8a72501e4129269
jason <potvinj@gmail.com>
2023-03-21 15:52:25 -0400

    set basic routes

--- client/Main.tsx
+++ client/Main.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import "./Main.css"
 import Homepage from "./components/Homepage";
+import AddContent from "./components/AddContent";
+import { Route, Routes } from "react-router-dom";
 
 const Main: React.FC = () => {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/newPost" element={<AddContent />} />
+      </Routes>
     </div>
   );
 };
