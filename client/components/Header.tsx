1f89b632dcd24323495f9e19cf7c958df0e5b405
jason <potvinj@gmail.com>
2023-03-21 15:53:36 -0400

    add temp links to pages

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Header: React.FC = () => {
     return (
         <header className='header'>
             <h1 className='header-title'>Blorgs Blog</h1>
+            <Link to="/">Home</Link>
+            <Link to="/newPost">New Post</Link>
             <button className='header-login-button'>Log In</button>
         </header>
     )
