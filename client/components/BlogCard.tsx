83c9fdb04d042cf814af6b7f26d8ebf24fe8069a
jason <potvinj@gmail.com>
2023-03-21 15:52:59 -0400

    fix bug form time formater

--- client/components/BlogCard.tsx
+++ client/components/BlogCard.tsx
@@ -20,12 +20,18 @@ const BlogCard: React.FC<Props> = ({ blogData }) => {
     day: "2-digit"
   });
 
+  const formatDate = (time: string) => { 
+    if (typeof time === "string") {
+      return formatter.format(Date.parse(time)) 
+    }
+  }
+
   return (
     <div className='blog-card'>
         <img src={ blogData.imgRef } alt='sample pic' className='thumbnail-pic'/>
         <div className='blog-card-text'>
             <h2>{ blogData.title }</h2>
-            <p className='date'>{ formatter.format(Date.parse(blogData.createdAt)) } | { blogData.category.toUpperCase() }</p>
+            <p className='date'>{ formatDate(blogData.createdAt) } | { blogData.category.toUpperCase() }</p>
             <p>{ blogData.description } <span><a href=''>Read More...</a></span></p>
         </div>
     </div>
