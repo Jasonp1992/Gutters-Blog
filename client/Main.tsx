4c7cd91c7cf512e40665076ace4bb6ad7327547c
jason <potvinj@gmail.com>
2023-04-05 16:30:12 -0400

    make sidebar global

--- client/Main.tsx
+++ client/Main.tsx
@@ -1,19 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Main.css"
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import AddContent from "./components/AddContent";
 import SingleBlog from "./components/SingleBlog";
+import hamburger from "../public/assets/Img/hamburger.png"
+import Sidebar from "./components/Sidebar";
 
 const Main: React.FC = () => {
+  const [sideBarOpen, setSideBarOpen] = useState<boolean>(false);
+
+  const showSidebar = () => {
+    setSideBarOpen(!sideBarOpen)
+  }
+
   return (
-    <div>
+    <>
+      <img className="hamburger-icon" src={hamburger} alt="black hamburger icon" onClick={showSidebar}/>
+        {
+            sideBarOpen ? (
+                <div className="sidebar-container" onClick={showSidebar}>
+                    <Sidebar />
+                </div>
+            ) : null
+        }
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/newPost" element={<AddContent />} />
         <Route path="/singlePost" element={<SingleBlog />} />
       </Routes>
-    </div>
+    </>
   );
 };
 
