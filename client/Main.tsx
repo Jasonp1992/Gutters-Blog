36d4148594d3ce6391b32e0de34b8bebf7c8e6a5
jason <potvinj@gmail.com>
2023-04-05 16:07:39 -0400

    add route for single post'

--- client/Main.tsx
+++ client/Main.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import "./Main.css"
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import AddContent from "./components/AddContent";
-import { Route, Routes } from "react-router-dom";
+import SingleBlog from "./components/SingleBlog";
 
 const Main: React.FC = () => {
   return (
@@ -10,9 +11,11 @@ const Main: React.FC = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/newPost" element={<AddContent />} />
+        <Route path="/singlePost" element={<SingleBlog />} />
       </Routes>
     </div>
   );
 };
 
 export default Main;
+
